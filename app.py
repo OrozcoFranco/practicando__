24a1165791a917e4cc46faf45049d42b368c3059
franco-orozco <franco.nahuel.orozco@gmail.com>
2024-11-20 19:48:24 -0300

    agregue funcion

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ num1 = 1
 num2 = 6
 resultado = num1 + num2
 print(resultado)
+
+def sumar(num1, num2):
+    resultado = num1 + num2
+    return resultado
+num1 = int(input("ingrese el primer numero: "))
+num2 = int(input("ingrese el segundo numero: "))
+print(sumar(num1, num2))
