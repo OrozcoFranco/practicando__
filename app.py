18da111f3ac7873d50443b2e69a83edac630216a
valeria-paredes <paredesvaleria1996@gmail.com>
2024-11-20 19:48:42 -0600

    agregue funcion

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ num1 = 1
 num2 = 6
 resultado = num1 + num2
 print(resultado)
+
+def sumar (num1, num2, num3):
+    resultado = num1 + num2 + num3
+    return resultado
+num1 = int(input("ingrese el primer numero:  "))
+num2 = int(input("ingrese el segundo numero: "))
+num3 = int(input("ingrese el tercer numero: "))
+
+print(sumar(num1, num2, num3))
